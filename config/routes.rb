1b1a669435c2880a8eec8d0c23c4c0576f57c9e8
Riosoft <riosoft@outlook.com>
2022-03-20 19:57:20 -0600

    Actualizacion home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
+
+  get "/cookies", to: "cookies#cookies"
+  get "/tarts", to: "tarts#tarts"
+  get "/cakes", to: "cakes#cakes"
+  get "/deserts", to: "deserts#deserts"
+  get "/breads", to: "breads#breads"
 end
---
+++ test/controllers/deserts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DesertsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
